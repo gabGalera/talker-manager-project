5a04c709eff98754b74f008d9c3993ac004ca307
Gabriel Galera <gabgalera@hotmail.com>
2023-02-06 14:55:03 -0300

    Req 2

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readJSON();
+  const talker = talkers.filter((data) => data.id === Number(id));
+  if (talker.length > 0) {
+    return res.status(200).json(talker[0]);
+  } 
+    return res.status(404).json({
+      message: 'Pessoa palestrante não encontrada',
+    });
+});
+
 app.get('/talker', async (_req, res) => {
   const data = await readJSON();
   if (data) {
