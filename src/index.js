c9bdc5739814e7d86cd030631f2cfc0f3918f511
Gabriel Galera <gabgalera@hotmail.com>
2023-02-06 15:26:27 -0300

    Req 4 antes de refatorar

--- src/index.js
+++ src/index.js
@@ -34,10 +34,28 @@ function generateToken() {
 
 app.post('/login', async (req, res) => {
   const { email, password } = req.body;
-  const token = generateToken();
-  return res.status(200).json({
-    token,
-  });
+  const tester = /\w+@\w+\.\w+/;
+  if (!email) {
+    return res.status(400).json({
+      message: 'O campo "email" é obrigatório',
+    });
+  } if (!tester.test(email)) {
+    return res.status(400).json({
+      message: 'O "email" deve ter o formato "email@email.com"',
+    });
+  } if (!password) {
+    return res.status(400).json({
+      message: 'O campo "password" é obrigatório',
+    });
+  } if (password.length < 6) {
+    return res.status(400).json({
+      message: 'O "password" deve ter pelo menos 6 caracteres',
+    });
+  } 
+    const token = generateToken();
+    return res.status(200).json({
+      token,
+    });
 });
 
 // não remova esse endpoint, e para o avaliador funcionar
