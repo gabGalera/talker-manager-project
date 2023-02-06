5d518d212e4fe0d9f59f402c9efa55277c5842a4
Gabriel Galera <gabgalera@hotmail.com>
2023-02-06 15:52:07 -0300

    Valida o campo age

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ async function validateName(req, res, next) {
     return res.status(400).json({
       message: 'O campo "name" é obrigatório',
     });
-  } if (name.length) {
+  } if (name.length < 3) {
     return res.status(400).json({
       message: 'O "name" deve ter pelo menos 3 caracteres',
     });
@@ -86,6 +86,10 @@ app.post('/talker',
   } if (!Number.isInteger(age)) {
     return res.status(400).json({
       message: 'O campo "age" deve ser um "number" do tipo inteiro',
+    });
+  } if (Number(age) < 18) {
+    return res.status(400).json({
+      message: 'A pessoa palestrante deve ser maior de idade',
     });
   }
 });
